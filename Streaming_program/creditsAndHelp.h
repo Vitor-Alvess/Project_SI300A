dec4b9d5560d6cb0767920efa55fed095a8acdc6
VitorAlvesss <20vitor11@gmail.com>
2024-06-05 20:33:58 -0300

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	modified:   Streaming_program/creditsAndHelp.h

--- Streaming_program/creditsAndHelp.h
+++ Streaming_program/creditsAndHelp.h
@@ -1,18 +1,20 @@
 #ifndef CREDITSANDHELP_H_
 #define CREDITSANDHELP_H_
 
-#include "menu.h"
-#include <vector>
+#include<iostream>
 
 using namespace std;
 
-class creditsAndHelp: public Menu{
+class creditsAndHelp{
     private:
+        string title;
         string message;
+        string end_msg;
         void setMessage(string message);
     public:
-        creditsAndHelp(string message, vector<string> &options, string title, string choice_msg = "Pressione qualquer tecla...");
+        creditsAndHelp(string message, string title, string end_msg = "Pressione qualquer tecla...");
         string getMessage();
+        const string &getTitle() const;
         void printMessage(string message);
         virtual ~creditsAndHelp();
 };
