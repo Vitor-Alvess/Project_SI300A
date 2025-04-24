04e5ca174fdd8124497d7eb6162ddbbb5500782a
Vitor-Alvess <20vitor11@gmail.com>
2025-04-23 23:47:40 -0300

    Atualizacao

---
+++ Streaming_program/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/config/memoryDBConnection.cpp.o"
+  "CMakeFiles/main.dir/src/config/memoryDBConnection.cpp.o.d"
+  "CMakeFiles/main.dir/src/config/serverDBConnection.cpp.o"
+  "CMakeFiles/main.dir/src/config/serverDBConnection.cpp.o.d"
+  "CMakeFiles/main.dir/src/config/utils.cpp.o"
+  "CMakeFiles/main.dir/src/config/utils.cpp.o.d"
+  "CMakeFiles/main.dir/src/controller/controller.cpp.o"
+  "CMakeFiles/main.dir/src/controller/controller.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/model/creditsAndHelp.cpp.o"
+  "CMakeFiles/main.dir/src/model/creditsAndHelp.cpp.o.d"
+  "CMakeFiles/main.dir/src/model/menu.cpp.o"
+  "CMakeFiles/main.dir/src/model/menu.cpp.o.d"
+  "CMakeFiles/main.dir/src/model/register.cpp.o"
+  "CMakeFiles/main.dir/src/model/register.cpp.o.d"
+  "CMakeFiles/main.dir/src/view/registerMemDAO.cpp.o"
+  "CMakeFiles/main.dir/src/view/registerMemDAO.cpp.o.d"
+  "CMakeFiles/main.dir/src/view/registerServerDAO.cpp.o"
+  "CMakeFiles/main.dir/src/view/registerServerDAO.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
